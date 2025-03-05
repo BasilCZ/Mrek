139d2139c221c97bd1f4d4e940e3c0afefb5ca4e
BasilCZ <ondrej.kucera@proprius.cz>
2025-03-05 21:00:50 +0100

    Přidána kontrola jestli mapa byla úspěšně načtena

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ import world.WorldMap;
 public class Main {
     public static void main(String[] args) {
         WorldMap wm = new WorldMap();
-        wm.loadMap();
-        Console c = new Console();
-        c.start();
-        System.out.println(wm.getWorld());
+        if(wm.loadMap()){
+            Console c = new Console();
+            c.start();
+            System.out.println(wm.getWorld());
+        } else {
+            System.out.println("Error: World map could not be loaded");
+        }
     }
 }
